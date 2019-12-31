94ec2fab098ef33ce88a5b4cf07d19781ae46b51
Danish Naseem <danishnaseem05@gmail.com>
2019-12-30 23:27:30 -0600

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import sys
 import time
 import queue
 
+
 def pythonWebServer(host, port):
     webServer = socket(AF_INET, SOCK_STREAM)
     webServer.setblocking(0)
@@ -14,7 +15,12 @@ def pythonWebServer(host, port):
     outputs = []
     message_queues = {}
 
-    print('Ready to serve on Port ' + str(port))
+    if(host == '' or host == "localhost"):
+        # LOG
+        print('Ready to serve on localhost:' + str(port))
+    else:
+        # LOG
+        print('Ready to serve on ' + str(host) + ":" + str(port))
 
     while inputs:
         readable, writable, exceptional = select.select(inputs, outputs, inputs, 0)
@@ -44,6 +50,7 @@ def pythonWebServer(host, port):
                 if next_msg:
                     filename = next_msg.decode("utf-8").split()[1]
                 else:
+                    #LOG
                     print("\nClient connection timed out. Exiting now...\n")
                     s.close()
                     time.sleep(3)
@@ -52,7 +59,9 @@ def pythonWebServer(host, port):
                 # 400 Bad Request HTTP response
                 if(filename == "/"):
                     s.send("HTTP/1.1 400 Bad Request\r\n".encode("utf-8"))
+                    #LOG
                     print("400 Bad Request")
+                    serverGUI.serverLogs("400 Bad Request")
                     s.close()
                     return
                     
@@ -60,16 +69,21 @@ def pythonWebServer(host, port):
                     try:
                         htmlFile = open(filename[1:]) # Throws IOError if not found
                         htmlFileContent = htmlFile.read() # HTML file content
+                        #LOG
                         print("\nCONTENTS OF THE WEBPAGE:")
+                        #LOG
                         print(htmlFileContent)
                         
                         fileLocation = process_http_header(webServer, filename)
+                        #LOG
                         print("\nFILE PATH: " + fileLocation)
                         
                         # Connection Succesfull, 200 OK HTTP response
                         s.send("HTTP/1.1 200 OK\r\n".encode("utf-8"))
-                        print("\n200 OK") 
+                        #LOG
+                        print("\n200 OK")
                         s.send("Content-Type: text/html\r\n\r\n".encode("utf-8"))
+                        #LOG
                         print("Content-type: text/html")
                         s.send(htmlFileContent.encode("utf-8"))
                         
@@ -78,11 +92,13 @@ def pythonWebServer(host, port):
                         filetoWrite.write(htmlFileContent)
                         filetoWrite.close()
 
+                        #LOG
                         print('\nReady to serve on Port ' + str(port))
 
                     # 404 Not Found HTTP response    
                     except IOError:
                         s.send("HTTP/1.1 404 Not Found\r\n".encode("utf-8"))
+                        #LOG
                         print("404 Not Found\n")
                         s.close()
                         return
